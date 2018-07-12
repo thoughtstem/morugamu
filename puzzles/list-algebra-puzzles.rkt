2d11d8c65568f400bf0b92c087e1ae7c36f5a342
Dhakshin <dhakshin01@gmail.com>
2018-07-12 13:05:59 -0700

    list-algebra-puzzle file

---
+++ puzzles/list-algebra-puzzles.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require
+  "../card-designs.rkt"
+  "../rules.rkt"
+  (prefix-in bool: "../boolean-algebra-cards.rkt")
+  (prefix-in bool: "../themes/emoji-boolean-algebra.rkt")
+  (prefix-in numb: "../clock-number-cards.rkt")  
+  (prefix-in numb: "../themes/emoji-clock-arithmetic.rkt")
+  (prefix-in ineq: "../inequality-cards.rkt")
+  (prefix-in list "../list-algebra-cards.rkt")
+  (prefix-in list "../themes/emoji-list-algebra.rkt"))
+
+(define clock-numbers
+  (numb:render numb:theme))
+
+(define boolean-algebra
+  (bool:render bool:theme))
+
+(define ineq-cards
+  (ineq:render #f))
+
+(define list-algebra-cards
+  (ineq:render #f))
+
+
+clock-numbers
+boolean-algebra
+ineq-cards
+list-algebra-cards
+
+
+
