740e0195f47a3fa08d1d295cde4dbbca5199d5a3
Marlon Aquino <marlon.aquino24@gmail.com>
2018-07-12 15:25:43 -0700

    fixed ruled cards and prefix arguments

--- puzzles/functions-list-algebra-puzzles.rkt
+++ puzzles/functions-list-algebra-puzzles.rkt
@@ -3,36 +3,36 @@
 (require
   "../card-designs.rkt"
   "../rules.rkt"
-  (prefix-in bool: "../boolean-algebra-cards.rkt")
-  (prefix-in bool: "../themes/emoji-boolean-algebra.rkt")
-  (prefix-in numb: "../clock-number-cards.rkt")  
-  (prefix-in numb: "../themes/emoji-clock-arithmetic.rkt")
-  (prefix-in ineq: "../inequality-cards.rkt")
-  (prefix-in listalg:"../list-algebra-cards.rkt")
+  ;(prefix-in bool: "../boolean-algebra-cards.rkt")
+  ;(prefix-in bool: "../themes/emoji-boolean-algebra.rkt")
+  ;(prefix-in numb: "../clock-number-cards.rkt")  
+  ;(prefix-in numb: "../themes/emoji-clock-arithmetic.rkt")
+  ;(prefix-in ineq: "../inequality-cards.rkt")
+  (prefix-in listalg: "../list-algebra-cards.rkt")
   (prefix-in listalg: "../themes/emoji-list-algebra.rkt")
-  (prefix-in functionlist:"../functions-list-algebra-cards.rkt")
-  (prefix-in functionlist: "../themes/emoji-functions-list-algebra.rkt"))
+  (prefix-in flist: "../functions-list-algebra-cards.rkt")
+  (prefix-in flist: "../themes/emoji-functions-list-algebra.rkt"))
 
-(define clock-numbers
+;(define clock-numbers
   (numb:render numb:theme))
 
-(define boolean-algebra
+;(define boolean-algebra
   (bool:render bool:theme))
 
-(define ineq-cards
-  (numb:render numb:theme))
+;(define ineq-cards
+  (ineq:render #f))
 
 (define list-algebra-cards
   (listalg:render listalg:theme))
 
 (define function-list-cards
-  (functionlist:render functionlist:theme))
+  (flist:render flist:theme ))
 
 
-clock-numbers
-boolean-algebra
-ineq-cards
-list-algebra-cards
+;clock-numbers
+;boolean-algebra
+;ineq-cards
+;list-algebra-cards
 function-list-cards
 
 
