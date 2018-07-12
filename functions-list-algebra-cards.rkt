bd82efc5c8c1df8a84de6d01aa46da4e5988342e
Marlon Aquino <marlon.aquino24@gmail.com>
2018-07-12 14:15:47 -0700

    moved everything back

---
+++ functions-list-algebra-cards.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(provide render)
+
+(require "./card-designs.rkt"
+         "./rules.rkt"
+
+         2htdp/image)
+
+
+(define (icon s)
+  (text s 50 "black"))
+
+(define (render theme)
+
+  (define-tile the-len 'len (first theme))
+  
+  (list
+   (symbol-page)
+
+  (rule-card (rule '(len (cons xy
+
+
