a3fe1e3c8297b55a2613dfb4c43937baa95662cd
Simon Xu <simon2017@Simons-MacBook-Pro.local>
2018-07-17 14:32:50 -0700

    changed the print-all.rkt file

--- print/print-all.rkt
+++ print/print-all.rkt
@@ -4,7 +4,6 @@
   2htdp/image
   "../rule-systems/card-designs.rkt"
   "../rule-systems/rules.rkt"
-
   "./printing-util.rkt"
   
   (prefix-in bool: "../rule-systems/boolean-algebra-cards.rkt")
@@ -16,14 +15,19 @@
   (prefix-in numb: "../puzzles/clock-number-puzzles.rkt")
 
   (prefix-in ineq: "../rule-systems/inequality-cards.rkt")
+  (prefix-in ineq: "../themes/emoji-inequality.rkt")
   (prefix-in ineq: "../puzzles/ineq-puzzles.rkt")
-  ;ineq has no theme yet.  TODO: refactor it to be like the others...
+  
 
   (prefix-in list: "../rule-systems/list-algebra-cards.rkt")
-  (prefix-in list: "../themes/emoji-list-algebra.rkt"))
+  (prefix-in list: "../themes/emoji-list-algebra.rkt")
+  (prefix-in list: "../puzzles/list-algebra-puzzles.rkt")
+
+  )
+
 
 (define puzzles
-  ;(cards->pages ;BROKEN???
+ 
   (list bool:puzzle-deck
         numb:puzzle-deck
         (rest (list:render list:theme))
@@ -32,26 +36,18 @@
 ))
 
 (define cards
-  ;(cards->pages ;BROKEN???
-   (number-all (append (rest (bool:render bool:theme))
+  (cards->pages
+   (list (number-all (append (rest (bool:render bool:theme))                 
                        (rest (numb:render numb:theme))
                        (rest (list:render list:theme))
-                       ;(rest (ineq:render #f)))) ineq has no themes so it throws an error for now -Taco
-  ;)
-)))
+                       )))))
 
+                      
 
 (define tiles
   (list (first (bool:render bool:theme))
          (first (numb:render numb:theme))
          (first (list:render list:theme))
-         ineq:render ))
+         )) 
 
 (append tiles cards puzzles)
-
-
-
-
-
-
-
