1d155a0e1d731e9d229e157cc3fa1d36d43f8759
Grace <gracie.m.832@gmail.com>
2018-07-17 14:20:09 -0700

    documenting, issue 19

--- puzzles/puzzle-util.rkt
+++ puzzles/puzzle-util.rkt
@@ -9,3 +9,11 @@
 
 (define (expression->puzzle-card exp)
   (puzzle-card (rule exp '?)))
+
+;(module+ test
+  ;(require "../rule-systems/boolean-algebra-cards.rkt")
+  ;(require "../themes/emoji-boolean-algebra.rkt")
+  ;(render theme)
+  
+  ;(expression->puzzle-card '(and (not T) F))
+  ;)
