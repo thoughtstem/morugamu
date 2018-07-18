82158d9214c79c6f1042c2e9d827a0a78f1c7997
Jacob <jlaxamana1298@gmail.com>
2018-07-18 14:52:22 -0700

    accidentally deleted too much last time

--- rule-systems/redex/boolean-algebra.rkt
+++ rule-systems/redex/boolean-algebra.rkt
@@ -81,6 +81,37 @@
 (module+ test (require rackunit))
 
 (define-language clock-numbers-lang
+  (compatible-closure
+   (reduction-relation
+    clock-numbers-lang
+
+    (--> (S 0) 1)
+    (--> (S 1) 2)
+    (--> (S 2) 3)
+    (--> (S 3) 4)
+    (--> (S 4) 5)
+    (--> (S 5) 6)
+    (--> (S 6) 7)
+    (--> (S 7) 8)
+    (--> (S 8) 9)
+    (--> (S 9) 0)
+    
+    (--> (P 0) 9)
+    (--> (P 1) 0)
+    (--> (P 2) 1)
+    (--> (P 3) 2)
+    (--> (P 4) 3)
+    (--> (P 5) 4)
+    (--> (P 6) 5)
+    (--> (P 7) 6)
+    (--> (P 8) 7)
+    (--> (P 9) 8)
+
+    (--> (add number_1 0)
+         number_1)
+    (--> (add number_1 number_2)
+         (add (S number_1) (P number_2))))
+   clock-numbers-lang e)
   (e   ::=
 
        (bop e e)
