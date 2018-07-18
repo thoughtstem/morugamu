71062f5154f172b22376841701c1edb93b01d74f
Grace <gracie.m.832@gmail.com>
2018-07-18 15:05:58 -0700

    this finally worked!

--- rule-systems/functions-list-algebra-cards.rkt
+++ rule-systems/functions-list-algebra-cards.rkt
@@ -7,6 +7,10 @@
 
          2htdp/image)
 
+(module+ test
+  (require (prefix-in numb: "../themes/emoji-functions-list-algebra.rkt"))
+
+  (render numb:theme))
 
 (define (icon s)
   (text s 50 "black"))
@@ -14,11 +18,24 @@
 (define (render theme)
 
   (define-tile len 'len
-    (bitmap "./themes/emojis/length.png"))
+    (bitmap "../themes/emojis/length.png"))
+  (define-tile cons 'cons
+    (bitmap "../themes/emojis/cons.png"))
+  (define-tile nil 'nil
+    (bitmap "../themes/emojis/nil.png"))
 
-  
-  (list
-   (symbol-page)
+  (define dot (third theme))
+  (define-tile the-1          '1    dot)
+  (define-tile the-3 '3
+    (above dot (beside dot dot)))
+  (define-tile the-prompt '?
+    (text "?" 40 "black"))
+
+
+(flatten (list
+   (get-rparen)
+   (get-lparen)
+   (get-all-symbols)
 
   
   (rule-card (rule '(len (cons x nil))
@@ -27,7 +44,7 @@
                          '3))
 
   (rule-card (rule '(len x y)
-                   '?))))
+                   '?)))))
 
 
 
