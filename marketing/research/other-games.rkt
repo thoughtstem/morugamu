25d2e4ebb5b48add2f42410fcbf6119ed03a0c58
Julia <juliabaylon01@gmail.com>
2018-07-19 15:00:18 -0700

    more games added

--- marketing/research/other-games.rkt
+++ marketing/research/other-games.rkt
@@ -5,14 +5,14 @@
 
 (plot (list (discrete-histogram
                '(#(Vanguard-of-War 20) #(Era-of-Kingdoms 8) #(Reign-Ed.2 35)
-                 #(Game-4 1) #(Game-5 3) #(Game-6 5)
+                 #(The-Game-of-Crimps 8.5) #(Game-to-Pick-a-Game 8) #(Crown-of-Aragon 7.5)
                  #(Game-7 4.5) #(Game-8 3) #(Game-9 2)
                  #(Game-10 2))
                #:skip 2 #:x-min 0
                #:label "$ Asked For")
               (discrete-histogram
                '(#(Vanguard-of-War 123) #(Era-of-Kingdoms 8.5) #(Reign-Ed.2 21)
-                 #(Game-4 2) #(Game-5 3.2) #(Game-6 2.1)
+                 #(The-Game-of-Crimps 2.8) #(Game-to-Pick-a-Game 17.7) #(Crown-of-Aragon 1.9)
                  #(Game-7 3) #(Game-8 4) #(Game-9 3)
                  #(Game-10 4))
                #:skip 2 #:x-min 1
