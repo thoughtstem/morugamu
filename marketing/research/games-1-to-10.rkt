ba19c5525831ff85bb39e131e42a2539f6baa55f
Julia <juliabaylon01@gmail.com>
2018-07-19 15:22:41 -0700

    fixed my bad math on games-1-to-10

--- marketing/research/games-1-to-10.rkt
+++ marketing/research/games-1-to-10.rkt
@@ -4,17 +4,17 @@
 (plot-new-window? #t)
 
 (plot (list (discrete-histogram
-               '(#(Vanguard-of-War 20) #(Era-of-Kingdoms 8) #(Reign-Ed.2 35)
-                 #(The-Game-of-Crimps 8.5) #(Game-to-Pick-a-Game 8) #(Crown-of-Aragon 7.5)
-                 #(Darkness-Incarnate 50) #(Absolutism 15) #(STARTROPOLIS 10)
-                 #(Miremarsh 58.8))
+               '(#(Vanguard-of-War 2) #(Era-of-Kingdoms 0.8) #(Reign-Ed.2 3.5)
+                 #(The-Game-of-Crimps 0.85) #(Game-to-Pick-a-Game 0.8) #(Crown-of-Aragon 0.75)
+                 #(Darkness-Incarnate 5) #(Absolutism 1.5) #(STARTROPOLIS 1)
+                 #(Miremarsh 5.9))
                #:skip 2 #:x-min 0
                #:label "$ Asked For")
               (discrete-histogram
-               '(#(Vanguard-of-War 123) #(Era-of-Kingdoms 8.5) #(Reign-Ed.2 21)
-                 #(The-Game-of-Crimps 2.8) #(Game-to-Pick-a-Game 17.7) #(Crown-of-Aragon 1.9)
-                 #(Darkness-Incarnate 0) #(Absolutism 3.9) #(STARTROPOLIS 36.9)
-                 #(Miremarsh 98.5))
+               '(#(Vanguard-of-War 12.3) #(Era-of-Kingdoms .85) #(Reign-Ed.2 2.1)
+                 #(The-Game-of-Crimps .28) #(Game-to-Pick-a-Game 1.8) #(Crown-of-Aragon .19)
+                 #(Darkness-Incarnate 0) #(Absolutism .39) #(STARTROPOLIS 3.69)
+                 #(Miremarsh 9.9))
                #:skip 2 #:x-min 1
                #:label "$ Received" #:color 2 #:line-color 2))
         #:x-label "Games" #:y-label "Money (in tens of thousands of dollars)"
