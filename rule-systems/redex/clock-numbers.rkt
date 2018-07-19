c7f4d427601cbc99461b1658db5e4f0adfc99cd9
Jacob <jlaxamana1298@gmail.com>
2018-07-19 15:44:12 -0700

    clock numbers redex works how it should now

--- rule-systems/redex/clock-numbers.rkt
+++ rule-systems/redex/clock-numbers.rkt
@@ -13,10 +13,14 @@
   (op  ::=
        S P)
   (bop ::=
-       add sub))
+       add sub)
+  (m number))
+
+#;(define-extended-language eval-clock-numbers-lang  clock-numbers-lang
+  (E hole (S E) (P E) (add n e) (sub n E) (sub E e)))
 
 (define-extended-language eval-clock-numbers-lang  clock-numbers-lang
-  (E hole (S E) (P E) (add n E) (add E e) (sub n E) (sub E e)))
+  (E hole (S E) (P E) (add e E) (add E e) (sub n E) (sub E e)))
 
 (define-metafunction eval-clock-numbers-lang
   S~ : n -> n
@@ -44,23 +48,42 @@
   [(P~ 8) 7]
   [(P~ 9) 8])
 
+(define-metafunction eval-clock-numbers-lang
+  add~ : n n -> e
+  [(add~ n_1 0) n_1]
+  [(add~ n_1 n_2) (add (S n_1) (P n_2))]
+    )
+
+(define-metafunction eval-clock-numbers-lang
+  sub~ : n n -> e
+  [(sub~ n_1 0) n_1]
+  [(sub~ n_1 n_2) (sub (P n_1) (P n_2))]
+   )
+
+
+
 (define clock-numbers-lang-red
   (reduction-relation
    eval-clock-numbers-lang
    #:domain e
-   (--> (in-hole E (S n)) (in-hole E (S~ n)) S)
+   (--> (in-hole E (S n)) (in-hole E (S~ n)) D)
    (--> (in-hole E (P n)) (in-hole E (P~ n)) P)
+   (--> (in-hole E (add n_1 n_2)) (in-hole E (add~ n_1 n_2)) A)
+   (--> (in-hole E (sub n_1 n_2)) (in-hole E (sub~ n_1 n_2)) S)
 
    ;Should implement these as meta-functions like S~ and P~ (add~, or~)
-   (--> (in-hole E (add 0 n_1))
+   #;(--> (in-hole E (add 0 n_1))
         (in-hole E n_1)
         B)
-   (--> (in-hole E (add n n_1))
+   #;(--> (in-hole E (add n n_1))
         (in-hole E (add (P n) (S n_1)))
-        (side-condition (> (term n) 0)) R)))
+        (side-condition (> (term n) 0)) R)
+   ))
+   
 
 
 
 (module+ test
   (traces clock-numbers-lang-red
-          (term (add 2 1))))
+          (term (add 4 (P (sub 9 (S 4)))))))
+
