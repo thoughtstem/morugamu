21732026affdd23a7749f4994c850213eeeb4c34
Holly Phan <HollyPhan@Hollys-MacBook-Air.local>
2018-07-20 13:40:42 -0700

    Added games 31-40

--- marketing/research/games-31-40.rkt
+++ marketing/research/games-31-40.rkt
@@ -4,15 +4,15 @@
 (plot-new-window? #t)
 
 (plot (list (discrete-histogram
-               '(#(g1 2) #(g2 0.8) #(g3 3.5)
-                 #(g4 0.85) #(g5 0.8) #(g6 0.75)
+               '(#(Dark-Matter 2) #(HeartBeat-Pride-Dice 0.8) #(Bat-Cup 3.5)
+                 #(Dragon-Realms 0.85) #(g5 0.8) #(g6 0.75)
                  #(g7 5) #(g8 1.5) #(g9 1)
                  #(g10 5.9))
                #:skip 2 #:x-min 0
                #:label "$ Asked For")
               (discrete-histogram
-               '(#(g1 2) #(g2 0.8) #(g3 3.5)
-                 #(g4 0.85) #(g5 0.8) #(g6 0.75)
+               '(#(Dark-Matter 1.3) #(HeartBeat-Pride-Dice 7.8) #(Bat-Cup 2.8)
+                 #(Dragon-Realms 0.85) #(g5 0.8) #(g6 0.75)
                  #(g7 5) #(g8 1.5) #(g9 1)
                  #(g10 5.9))
                #:skip 2 #:x-min 1
