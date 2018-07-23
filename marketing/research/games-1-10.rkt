58625c418948bcd976170bba9b798491b91d239d
Marlon Aquino <marlon.aquino24@gmail.com>
2018-07-23 15:28:54 -0700

    Added space in between bars

--- marketing/research/games-1-10.rkt
+++ marketing/research/games-1-10.rkt
@@ -9,14 +9,14 @@
                  #(The-Game-of-Crimps 0.85) #(Game-to-Pick-a-Game 0.8) #(Crown-of-Aragon 0.75)
                  #(Darkness-Incarnate 5) #(Absolutism 1.5) #(STARTROPOLIS 1)
                  #(Miremarsh 5.9))
-               #:skip 2 #:x-min 0
+               #:skip 3 #:x-min 0
                #:label "$ Asked For")
               (discrete-histogram
                '(#(Vanguard-of-War 12.3) #(Era-of-Kingdoms .85) #(Reign-Ed.2 2.1)
                  #(The-Game-of-Crimps .28) #(Game-to-Pick-a-Game 1.8) #(Crown-of-Aragon .19)
                  #(Darkness-Incarnate 0) #(Absolutism .39) #(STARTROPOLIS 3.69)
                  #(Miremarsh 9.9))
-               #:skip 2 #:x-min 1
+               #:skip 3 #:x-min 1
                #:label "$ Received" #:color 2 #:line-color 2))
         #:x-label "Games" #:y-label "Money (in tens of thousands of dollars)"
         #:title "Board Games Funded Via Kickstarter"))
