017531aa2c280f8f305dfa85386533e79d17a65c
danielflohr <danielflohr00@gmail.com>
2018-07-24 14:48:54 -0700

    working getIndex function

--- marketing/research/database-util.rkt
+++ marketing/research/database-util.rkt
@@ -1,1 +1,18 @@
 #lang racket
+
+(define lst
+(list '("a" 1) '("b" 1) '("c" 1) '("d" 1)))
+
+(define (get-index game)
+(define (recSearch n query)
+(if (> n (length lst))
+-1
+  '())
+(if (string=? (string-downcase query) (string-downcase (list-ref (list-ref lst n) 0)))
+n
+(recSearch (+ n 1) query))
+)
+(recSearch 0 game)
+)
+
+(get-index "d")
