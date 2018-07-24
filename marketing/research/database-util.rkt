4581ae21db5763298d155aaeff273a89b87ae2e4
tranj8229 <tranjulie@berkeley.edu>
2018-07-24 14:58:27 -0700

    Update database-util.rkt

--- marketing/research/database-util.rkt
+++ marketing/research/database-util.rkt
@@ -12,4 +12,28 @@
   (recSearch 0 game)
 )
 
+; Query Function #1: received >= asked
+(define (received-more data)
+	(cond
+          [(null? data) '()]
+          [else (define elem1 (first data))
+                (if (>= (list-ref elem1 2) (list-ref elem1 1))
+                    (cons elem1 (received-more (rest data)))
+                    (received-more (rest data))
+                )]
+         )
+)
+
+; Query Function #2: received >= $1,000,000
+(define (greater-than-million data)
+	(cond
+          [(null? data) '()]
+          [else (define elem1 (first data))
+                (if (>= (list-ref elem1 2) 100)  ; units are in tens of thousands
+                    (cons elem1 (greater-than-million (rest data)))
+                    (greater-than-million (rest data))
+                )]
+         )
+)
+
 (get-index "Dinosaur Island" board-game-list)
