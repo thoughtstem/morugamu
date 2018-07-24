01c3c35f4a0f90ac83bbbcc582ac3b3a7c3537fa
tranj8229 <tranjulie@berkeley.edu>
2018-07-24 14:57:08 -0700

    Update database-util.rkt

--- marketing/research/database-util.rkt
+++ marketing/research/database-util.rkt
@@ -1,18 +1,15 @@
 #lang racket
 
-(define lst
-(list '("a" 1) '("b" 1) '("c" 1) '("d" 1)))
-
-(define (get-index game)
-(define (recSearch n query)
-(if (> n (length lst))
--1
-  '())
-(if (string=? (string-downcase query) (string-downcase (list-ref (list-ref lst n) 0)))
-n
-(recSearch (+ n 1) query))
-)
-(recSearch 0 game)
+(define (get-index game lst)
+  (define (recSearch n query)
+    (if (> n (length lst))
+    -1
+      '())
+    (if (string=? (string-downcase query) (string-downcase (list-ref (list-ref lst n) 0)))
+    n
+    (recSearch (+ n 1) query))
+    )
+  (recSearch 0 game)
 )
 
-(get-index "d")
+(get-index "Dinosaur Island" board-game-list)
