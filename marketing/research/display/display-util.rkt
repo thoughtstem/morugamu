cb6719ee2740a166e161ed25c8d87d60fab156a3
JavaCafe01 <JavaCafe01@users.noreply.github.com>
2018-07-25 14:25:28 -0700

    added comments

--- marketing/research/display/display-util.rkt
+++ marketing/research/display/display-util.rkt
@@ -1,11 +1,14 @@
 #lang racket
 
 (require
+  racket/contract
   plot
   "../../../util/util.rkt")
 
 (provide data->histogram)
 
+;Parameters: list of vectors, number, number, string, number, number
+;Return: discrete histogram
 (define (data->histogram data-vector skip-num x-min label color line-color)
   (discrete-histogram
                data-vector
