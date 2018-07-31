744fe49a15afca10127097bbcf157ce71252facd
Jacob <jlaxamana1298@gmail.com>
2018-07-31 15:38:48 -0700

    a couple more graphs

--- marketing/research/display/display.rkt
+++ marketing/research/display/display.rkt
@@ -5,7 +5,7 @@
   "display-util.rkt"
   "../../../util/util.rkt"
   (prefix-in game: "../data/games/database.rkt")
-  "../data/stephens-database-util.rkt"
+  (prefix-in util:"../data/stephens-database-util.rkt")
    )
 
 ;Plotting Games Database
@@ -17,6 +17,24 @@
 
 (plot-new-window? #t)
 
+;plotting successful games
+(define vector-success-asked
+  (map list->vector (elements->lst (util:Success? game:$received game:$asked-for (game:table)) 1 2)
+  ))
+
+(define vector-success-received
+  (map list->vector (elements->lst (util:Success? game:$received game:$asked-for (game:table)) 1 3)
+  ))
+
+;plotting unsuccessful games
+(define vector-unsuccess-asked
+  (map list->vector (elements->lst (util:Success? game:$asked-for game:$received (game:table)) 1 2)
+  ))
+
+(define vector-unsuccess-received
+  (map list->vector (elements->lst (util:Success? game:$asked-for game:$received (game:table)) 1 3)
+  ))
+
 (parameterize ([plot-x-tick-label-anchor 'top-right]
                [plot-x-tick-label-angle 50])
 
@@ -25,3 +43,22 @@
        (data->histogram vector-received 3 1 "$ Received" 2 2))
         #:x-label "Games" #:y-label "Money (in tens of thousands of dollars)"
         #:title "Board Games Funded Via Kickstarter"))
+
+(parameterize ([plot-x-tick-label-anchor 'top-right]
+               [plot-x-tick-label-angle 50])
+
+(plot
+ (list (data->histogram vector-success-asked 3 0 "$ Asked For" 0 0)
+       (data->histogram vector-success-received 3 1 "$ Received" 2 2))
+        #:x-label "Games" #:y-label "Money (in tens of thousands of dollars)"
+        #:title "Board Games Funded Via Kickstarter and Succeeded"))
+
+
+(parameterize ([plot-x-tick-label-anchor 'top-right]
+               [plot-x-tick-label-angle 50])
+
+(plot
+ (list (data->histogram vector-unsuccess-asked 3 0 "$ Asked For" 0 0)
+       (data->histogram vector-unsuccess-received 3 1 "$ Received" 2 2))
+        #:x-label "Games" #:y-label "Money (in tens of thousands of dollars)"
+        #:title "Board Games Funded Via Kickstarter and did not Succeeded"))
