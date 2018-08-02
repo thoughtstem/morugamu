8a9c6c7e2bfccb80253d682a37b3ede73ea86803
Chloe <nchloe99@gmail.com>
2018-08-02 15:03:04 -0700

    i needed help

--- marketing/research/display/bubble/bubble.rkt
+++ marketing/research/display/bubble/bubble.rkt
@@ -1,17 +1,19 @@
 #lang racket
 
-(require 2htdp/image)
-(require (prefix-in game: "./games/database.rkt"))
-(require (prefix-in company: "./companies/database.rkt"))
-(require (prefix-in companies<->games: "./join-games-companies/database.rkt"))
+(require (prefix-in game: "../../data/games/database.rkt"))
+(require (prefix-in company: "../../data/companies/database.rkt"))
+(require (prefix-in companies<->games: "../../data/join-games-companies/database.rkt"))
 (require plot)
+(require "../../../../util/util.rkt")
 
-(plot
-    (points (for/list ([i (in-range 1000)])
-              (list 0 0))
-            #:alpha 0.4
-            #:x-jitter 1
-            #:y-jitter 1
-            #:sym 'fullcircle1
-            #:color "blue")
-    #:x-min -2 #:x-max 2 #:y-min -2 #:y-max 2)
+(plot-new-window? #t)
+
+(define vector-games
+  (map list->vector (elements->lst (game:table) 2 3)))
+
+(plot (points vector-games))
+
+
+
+
+
