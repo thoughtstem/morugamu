cda9b489a16a5b6f874cd925ad0f08eeee639450
Julia <juliabaylon01@gmail.com>
2018-08-02 13:12:42 -0700

    added cool gui demo from the documentation

--- marketing/research/display/venn/venn.rkt
+++ marketing/research/display/venn/venn.rkt
@@ -1,3 +1,28 @@
 #lang racket
 (require 2htdp/image)
+(require htdp/gui)
 
+; text1 : GUI-ITEM...i.e. the text field
+(define text1
+  (make-text "Please enter your name"))
+ 
+; msg1 : GUI-ITEM
+(define msg1
+  (make-message (string-append "Hello, World" (make-string 33 #\space))))
+ 
+; Event -> true
+; draws the current contents of text1 into msg1, prepended with "Hello, "
+(define (respond e)
+  (draw-message msg1 (string-append "Hello, " (text-contents text1))))
+
+ 
+; set up window with three "lines":
+;    a text field, a message, and two buttons
+; fill in text and click OKAY
+(define w
+  (create-window
+   (list
+    (list text1)
+    (list msg1)
+    (list (make-button "OKAY" respond)
+          (make-button "QUIT" (lambda (e) (hide-window w)))))))
